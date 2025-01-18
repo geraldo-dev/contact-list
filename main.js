f808c391aae5c4e0e676f8671eb410ce46c5d2d2
geraldo ferreira <geraldosousadev@gmail.com>
2025-01-18 19:11:08 -0300

    function register new contact

--- main.js
+++ main.js
@@ -3,14 +3,20 @@ const phone = document.querySelector('#phone');
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const mensagens = document.querySelector('#mensagens');
+let count_id = 0;
 
 function alert_mensagen(msg, color){
-    mensagens.textContent = msg;
     mensagens.style.backgroundColor = color;
+    mensagens.style.color = 'white';
+    mensagens.textContent = msg;
 
     setTimeout(() => {
         mensagens.textContent = '';
     }, 2000);
+}
+
+function delete_contact(id){
+
 }
 
 function created (e){
@@ -18,6 +24,24 @@ function created (e){
     
     if(username.value === '' || phone.value === ''){
         alert_mensagen('preencha todos os campos', 'red');
+    }else{
+        //creating li tag
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+
+        //adding property to tags
+        li.setAttribute('id', count_id);
+        li.textContent = `${username.value} phone: ${phone.value}`;
+        button.setAttribute('onclick', `delete_contact(${count_id})`);
+        button.textContent = 'deletar';
+
+        //adding tags to parent tags
+        li.appendChild(button);
+        ul.appendChild(li);
+
+        count_id++;
+
+        alert_mensagen('salvo com sucesso', 'green');
     }
 }
 
